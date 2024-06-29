9803b58f2c483f06ba82d4ef057ca3a6ded03d84
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-06-29 16:04:24 +0200

    WIP: Checkpoint before refactor

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 //..ooOO00OOoo....ooOO00OOoo....ooOO00OOoo....ooOO00OOoo..
 //
-// Simple numpy like example with pybind11. 
-// This is a main c++11 application, there is an equivalent 
+// Simple numpy like example with pybind11.
+// This is a main c++11 application, there is an equivalent
 // version for python
 // Copyright 2016 Jimmy Aguilar Mena <spacibba@yandex.com>
 //
@@ -9,27 +9,28 @@
 
 #include "cnumpy.h"
 
-int main(){
-    cmatrix a(5,6,'r'), b(6,7,'r');
+int main()
+{
+	cmatrix a(5, 6, 'r'), b(6, 7, 'r');
 
-    cout<<"random a:\n"<<a<<endl;
-    cout<<"random b:\n"<<b<<endl;
+	cout << "random a:\n" << a << endl;
+	cout << "random b:\n" << b << endl;
 
-    cmatrix c=a*b;
-    cout<<"c=a*b:\n"<<c<<endl;
-    
-    b=a;
-    cout<<"b=a:\n"<<b<<endl;
-    
-    cmatrix d(a);
-    cout<<"d(a):\n"<<d<<endl;
-    
-    cout<<"d[1][2]: "<<d[1][2]<<endl<<endl;
-    
-    cout<<"d*3.0\n"<<d*3.<<endl;
+	cmatrix c = a * b;
+	// cout << "c=a*b:\n" << c << endl;
 
-    cout<<"3*d\n"<<3*d<<endl;
+	b = a;
+	cout << "b=a:\n" << b << endl;
 
-    d[1][2]=3.14;
-    cout<<"d[1][2]=3.14\n"<<d<<endl;
-    }
+	cmatrix d(a);
+	cout << "d(a):\n" << d << endl;
+
+	cout << "d[1][2]: " << d[1][2] << endl << endl;
+
+	cout << "d*3.0\n" << d * 3. << endl;
+
+	cout << "3*d\n" << 3 * d << endl;
+
+	d[1][2] = 3.14;
+	cout << "d[1][2]=3.14\n" << d << endl;
+}
