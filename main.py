f67ccb841bf07c515bb2c60a817bc615de83acac
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-06-29 21:35:59 +0200

    Update API
    After many years I take this project with big changes:
    1. Use Eigen instead of my own matrix and vector classes.
    2. Remove the pybind copied 8 years ago. An search the one in the system
    3. Use directly the Eigen class to expose
    4. Update the CMakeLists.txt to search Eigen and pybind

--- main.py
+++ main.py
@@ -9,60 +9,60 @@
 #
 #..ooOO00OOoo....ooOO00OOoo....ooOO00OOoo....ooOO00OOoo..
 
-import libcnumpy as my
+import EigenNumpy as enp
 import numpy as np
 
-a,b = my.cmatrix(5,6,"r"), my.cmatrix(6,7,"r"),
+a,b = enp.RowMatrix(5,6), enp.RowMatrix(6,7),
 
 print("random a:\n",a)
 print("random b:\n",b)
 
-c=a*b
+c = a * b
 print("c=a*b:\n",c)
 
-b=a
+b = a
 print("b=a:\n",b)
 
-d=my.cmatrix(a)
+d = enp.RowMatrix(a)
 print("d=cmatrix(a):\n",d)
 
-print("d[1,2]: ",d[1,2])
+# print("d[1,2]: ",d[1,2])
 
-print("d*3.0\n",d*3.0)
+# print("d*3.0\n",d*3.0)
 
-print("3*d\n",3*d)
+# print("3*d\n",3*d)
 
-print("=== Test Numpy ===\n")
+# print("=== Test Numpy ===\n")
 
-npa=np.random.sample((3,4))
+# npa=np.random.sample((3,4))
 
-print("Numpy array npa=np.random.sample((3,4)) (random numpy array)\n",npa)
+# print("Numpy array npa=np.random.sample((3,4)) (random numpy array)\n",npa)
 
-e=my.cmatrix(npa)
+# e=my.cmatrix(npa)
 
-print("\ne=my.cmatrix(npa) (copy from numpy array copy=False)\n",e)
+# print("\ne=my.cmatrix(npa) (copy from numpy array copy=False)\n",e)
 
-f=np.array(d)
+# f=np.array(d)
 
-print("f=np.array(d) (Copy to numpy)\n",f)
-
-
-print("d[1:-1:2,1:-1:2]\n",d[1:-1:2,1:-1:2])
+# print("f=np.array(d) (Copy to numpy)\n",f)
 
 
-# Test cvector
+# print("d[1:-1:2,1:-1:2]\n",d[1:-1:2,1:-1:2])
 
-a=my.cvector(8,"r")
-print("a=my.cvector(8,\"r\")\n",a)
 
-b=my.cvector(a)
-print("b=my.cvector(a)\n",b)
+# # Test cvector
 
-b=my.cvector(a,True)
-print("b=my.cvector(a,True)\n",b)
+# a=my.cvector(8,"r")
+# print("a=my.cvector(8,\"r\")\n",a)
 
-print("d[1:2,:]\n",d[1:2,:])
+# b=my.cvector(a)
+# print("b=my.cvector(a)\n",b)
 
-h=my.cmatrix(d[1:2,:])
-print("h=cmatrix(d[1:2,:])\n",h)
+# b=my.cvector(a,True)
+# print("b=my.cvector(a,True)\n",b)
+
+# print("d[1:2,:]\n",d[1:2,:])
+
+# h=my.cmatrix(d[1:2,:])
+# print("h=cmatrix(d[1:2,:])\n",h)
 
